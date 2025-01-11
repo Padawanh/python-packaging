6edf93878771708ef2cd8f362921c3623af106f8
Carlos Silva <chpsilva@outlook.com>
2025-01-11 19:52:57 -0300

    Update setup.py to read dependencies from requirements.txt and change version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ def read_file(file_name):
         return file.read()
 
 
+def read_requirements(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read().splitlines()
+
 setup(
     name="udemycalculator",
-    version="1.0.1",
+    version="1.0.0",
     packages=find_packages(include=['udemyCalculator', 'udemyCalculator.*']),
     url="",
     author="Idan Chen",
     description="This is my udemy calculator package",
     long_description=read_file(file_name="README.md"),
     python_requires=">=3.6",
-    install_requires=[
-        'numpy'
-    ]
+    install_requires=read_requirements(file_name="requirements.txt")
 )
